5a00afa835ea76db974362ea81b69d8eef57593d
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-09-04 18:38:35 +0300

    Information is added to the table with js function addRowToTable

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function windowOnClick(event) {
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+function addRowToTable() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+
+    cell1.innerHTML = document.getElementById("fname").value;
+    cell2.innerHTML = document.getElementById("lname").value;
+    cell3.innerHTML = document.getElementById("email").value;
+    cell4.innerHTML = document.getElementById("gender").value;
+    cell5.innerHTML = document.getElementById("birthday").value;
+    cell6.innerHTML = document.getElementById("image").value;
+  }
