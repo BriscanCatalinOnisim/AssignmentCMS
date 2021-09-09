267e196aee860dad528a6d2503673a5b9336e22b
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-09-09 12:26:30 +0300

    Readme.md updated

--- js/script.js
+++ js/script.js
@@ -38,11 +38,7 @@ function AppendTable(employee) {
     <td>${employee.email}</td>
     <td>${employee.gender}</td>
     <td>${employee.birthDate}</td>
-<<<<<<< HEAD:js/script.js
-    <td><img src="../${employee.picture.name}" width="50" height="60" class="picture"></td>
-=======
-    <td><img src="${employee.picture}" width="50" height="60" class="picture"></td>
->>>>>>> 5f1453d4b1f4f22008fedc06eee850408bfc507b:script.js
+    <td><img src="${employee.picture.name}" width="50" height="60" class="picture"></td>
     <td class="delete"> <input type="button" value="Delete Row"></td>
     </tr>`
     console.log(employee);
