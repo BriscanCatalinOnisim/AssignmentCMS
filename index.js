f99989562b02f22c24164d404a4db8f169c8e7b1
LAPTOP-2GI4N8QM\c_bri <catalin.briscan@principal3.com>
2021-09-13 13:11:54 +0300

    Sort button asc desc and background color

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-import {getFirestore, doc, setDoc, deleteDoc, getDocs, collection} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
+import {getFirestore, doc, setDoc, deleteDoc, getDocs, collection, query, where, orderBy} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -17,6 +17,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const querySnapshot = await getDocs(collection(db, "users"));
 const membersRef = collection(db, "users");
+var employeesTable = document.getElementById('myTable');
 
 function WriteMember(){
   var i=0;
@@ -164,6 +165,35 @@ function AddEmployee() {
   }
 }
 
+function AddEmployeeSort(employee) {
+  var lastName = employee.lastName;
+  var firstName = employee.firstName;
+  var email = employee.email;
+  var gender = employee.gender;
+  var birthday = employee.birthday;
+  var date = moment(birthday).format('D MMMM, YYYY');
+
+  var table=document.getElementById('myTable');
+  var row=table.insertRow();
+  row.className="bottom-row";
+  var cell1=row.insertCell(0);
+  var cell2=row.insertCell(1);
+  var cell3=row.insertCell(2);
+  var cell4=row.insertCell(3);
+  var cell5=row.insertCell(4);
+  var cell7=row.insertCell(5);
+  cell1.innerHTML=lastName;
+  cell2.innerHTML=firstName;
+  cell3.innerHTML=email;
+  cell4.innerHTML=gender;
+  cell5.innerHTML=date; 
+  cell7.innerHTML='<input type="button" class="delete-button fa fa-remove" id="deleteButton" value="X">'
+  var drop=document.getElementsByClassName("delete-button");
+  drop[row.rowIndex-1].addEventListener("click", async function(){
+    var currentid=document.getElementById('myTable').rows.length-1;
+    DeleteMember(row, currentid);
+  });
+}
 
 function DeleteMemberFromDatabase(currentid){
   deleteDoc(doc(db, "users", `${currentid}`));
@@ -177,7 +207,7 @@ function DeleteMember(row, currentid){
     table.deleteRow(id);
     DeleteMemberFromDatabase(currentid);
   }
-  
+
 }
 
 function validate(lastName, firstName, email, sex, birthday) {
@@ -212,6 +242,86 @@ function validate(lastName, firstName, email, sex, birthday) {
       return false;
   }
   return true;
+}
+
+const sort = document.querySelector(".sort");
+sort.addEventListener("click", sortTable);
+
+function removeDataFromViewTable(employeesTable) {
+  for (var i = employeesTable.childNodes[1].childElementCount - 1; i > 0; i--) {
+    console.log("fsdgfxcg");
+    employeesTable.deleteRow(i);
+  }
+}
+
+function Employee(employeeId, firstName, lastName, email, gender, birthday) {
+  this.employeeId = employeeId;
+  this.lastName = lastName;
+  this.firstName = firstName;
+  this.email = email;
+  this.birthday = moment(birthday).format("D MMMM, YYYY");
+  this.gender = gender;
+}
+
+function putData(querySnapshot) {
+  removeDataFromViewTable(employeesTable);
+
+  querySnapshot.forEach(element => {
+      var employee = new Employee(
+        element.data()["employeeId"],
+        element.data()["firstname"],
+        element.data()["lastname"],
+        element.data()["email"],
+        element.data()["gender"],
+        element.data()["birthday"]
+      ); 
+      AddEmployeeSort(employee);
+    });
+}
+
+async function sortTable() {
+  var sortBy = document.getElementById("sortBy").value;
+  var sortType = document.getElementById("sortType").value;
+  console.log(sortBy);
+  removeDataFromViewTable(employeesTable);
+  if (sortBy == "name") {
+    if (sortType == "asc") {
+      const q = query(
+        collection(db, "users"),
+        orderBy("lastname", "asc")
+      );
+      const querySnapshot = await getDocs(q);
+      document.getElementById("employeesTableBody").innerHTML = "";
+      putData(querySnapshot);
+    } else {
+      const q = query(
+        collection(db, "users"),
+        orderBy("lastname", "desc")
+      );
+      const querySnapshot = await getDocs(q);
+      document.getElementById("employeesTableBody").innerHTML = "";
+      console.log(querySnapshot);
+      putData(querySnapshot);
+    }
+  } else if (sortBy == "birthday") {
+    if (sortType == "asc") {
+      const q = query(
+        collection(db, "users"),
+        orderBy("birthday", "asc")
+      );
+      const querySnapshot = await getDocs(q);
+      document.getElementById("employeesTableBody").innerHTML = "";
+      putData(querySnapshot);
+    } else {
+      const q = query(
+        collection(db, "users"),
+        orderBy("birthday", "desc")
+      );
+      const querySnapshot = await getDocs(q);
+      document.getElementById("employeesTableBody").innerHTML = "";
+      putData(querySnapshot);
+    }
+  }
 }
 
 function validateEmail(email) 
